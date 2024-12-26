de7471c1e578fe59d00538c8a5e5e57c3388beba
iamtwobe <danielnm05@gmail.com>
2024-12-26 11:24:47 -0300

    lunch break (updated database_handler)

--- src/database/database_handler.py
+++ src/database/database_handler.py
@@ -50,14 +50,96 @@ class AppDatabase():
             """
         )
 
+    def insert_product(self, product_name=None, product_category=None, product_price=None, product_brand=None, product_description=None):
+        if not all([product_name, product_category, product_price, product_brand]):
+            return "Missing required fields"
+        
+        try:
+            self.cursor.execute(
+                "SELECT category_id FROM category WHERE category_name = ?",
+                (product_category.lower(),)
+            )
+            category_id = self.cursor.fetchone()
+            if not category_id:
+                print("Category does not exist")
+                return None
+            
+            self.cursor.execute(
+                "SELECT brand_id FROM brand WHERE brand_name = ?",
+                (product_brand.lower(),)
+            )
+            brand_id = self.cursor.fetchone()
+            if not brand_id:
+                print("Brand does not exist")
+                return None
+
+            self.cursor.execute(
+                """
+                    INSERT INTO products (product_name, product_category, product_price, product_brand, product_description)
+                    VALUES (?, ?, ?, ?, ?)
+                """,
+                (product_name.lower(), product_category.lower(), product_price, product_brand.lower(), product_description)
+            )
+            self.conn.commit()
+        except sqlite.IntegrityError:
+            print("Product already exists")
+            return None
+
+    def insert_category(self, category_name: str=None) -> None:
+        if not category_name:
+            print("Missing category_name")
+            return None
+        
+        try:
+            self.cursor.execute("INSERT INTO category (category_name) VALUES (?)", (category_name.lower(),))
+            self.conn.commit()
+        except sqlite.IntegrityError:
+            print("Category already exists")
+            return None
+        
+    def insert_brand(self, brand_name: str=None) -> None:
+        if not brand_name:
+            print("Missing brand_name")
+            return None
+        try:
+            self.cursor.execute("INSERT INTO brand (brand_name) VALUES (?)", (brand_name.lower(),))
+            self.conn.commit()
+        except sqlite.IntegrityError:
+            print("Brand already exists")
+            return None
+
+    def get_all_table(self, table) -> list:
+        table_data = self.cursor.execute(f"SELECT * FROM {table}")
+        selection = table_data.fetchall()
+        return self.verify_db(selection)
+    
+    def verify_db(self, selection) -> dict:
+        if selection:
+            # Se for mais que um item
+            if type(selection) is list:
+                results = []
+                for i in selection:
+                    column_names = [description[0] for description in self.cursor.description]
+                    result = dict(zip(column_names, i))
+                    results.append(result)
+                return results
+            # Se for apenas um item
+            column_names = [description[0] for description in self.cursor.description]
+            result = dict(zip(column_names, selection))
+            return result
+        else:
+            return None
+
     def close(self):
         self.conn.close()
-        
-    def __del__(self):
-        self.conn.close()
 
-inventory_db = AppDatabase("src/database/inventory.db")
 
 if __name__ == "__main__":
+    inventory_db = AppDatabase("src/database/inventory.db")
 
+    # inventory_db.insert_category("foods")
+    # inventory_db.insert_brand("good apples")
+    # inventory_db.insert_product("Apple", "foods", 0.99, "good apples", "An apple.")
+    query = inventory_db.get_all_table('brand')
+    print(query)
     inventory_db.close()
